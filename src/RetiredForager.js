6b449e24cb9f2a51b9bf80259593a21790562fed
Hack Reactor Student <student@hackreactor.com>
2017-05-15 11:41:03 -0500

    Finished Bees^3

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,14 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
 };
-
+RetiredForagerBee.prototype = Object.create(Grub.prototype);
+RetiredForagerBee.prototype.forage = function() {
+  // body...
+  return "I am too old, let me play cards instead";
+}
+RetiredForagerBee.prototype.gamble = ForagerBee.prototype.forage;
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
